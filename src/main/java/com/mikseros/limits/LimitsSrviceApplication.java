2f3d1121b08feb62b71eed8707092261a38e0c77
mikseros <mmadynski@gmail.com>
2022-09-08 20:35:35 +0200

    Initial commit (limits-service created)

---
+++ src/main/java/com/mikseros/limits/LimitsSrviceApplication.java
@@ -0,0 +1,13 @@
+package com.mikseros.limits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitsSrviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitsSrviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mikseros/limits/LimitsSrviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikseros.limits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsSrviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
